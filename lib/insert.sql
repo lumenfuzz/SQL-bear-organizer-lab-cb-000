6c694dab0d4784b99dda9f6a6f02791444e669ad
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-17 12:49:24 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,9 @@
-INSERT INTO bears (name, gender) VALUES
-('Mr. Chocolate', 0);
-INSERT INTO bears (name, gender) VALUES
-('Rowdy', 0);
-INSERT INTO bears (name, gender, age) VALUES
-('Tabitha', 1, 6);
-INSERT INTO bears (name, gender) VALUES
-('Sergeant Brown', 0);
-INSERT INTO bears (name, gender, age) VALUES
-('Melissa', 1, 13);
-INSERT INTO bears (name, gender) VALUES
-('Grinch', 0);
-INSERT INTO bears (name, gender, age) VALUES
-('Wendy', 1, 6);
-INSERT INTO bears (name) VALUES
-(NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
