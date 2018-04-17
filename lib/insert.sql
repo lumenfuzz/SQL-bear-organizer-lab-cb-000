f7c408bdde30178a3de98beceb892baf2dc59414
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-17 12:42:16 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,15 +2,15 @@ INSERT INTO bears (name, gender) VALUES
 ('Mr. Chocolate', 0);
 INSERT INTO bears (name, gender) VALUES
 ('Rowdy', 0);
-INSERT INTO bears (name, gender) VALUES
-('Tabitha', 1);
+INSERT INTO bears (name, gender, age) VALUES
+('Tabitha', 1, 6);
 INSERT INTO bears (name, gender) VALUES
 ('Sergeant Brown', 0);
-INSERT INTO bears (name, gender) VALUES
-('Melissa', 1);
+INSERT INTO bears (name, gender, age) VALUES
+('Melissa', 1, 13);
 INSERT INTO bears (name, gender) VALUES
 ('Grinch', 0);
-INSERT INTO bears (name, gender) VALUES
-('Wendy', 1);
+INSERT INTO bears (name, gender, age) VALUES
+('Wendy', 1, 6);
 INSERT INTO bears (name) VALUES
 (NULL);
